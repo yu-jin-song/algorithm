3064834c4daf6a989f40d726b203ceb7eeca4a79
Yujin Song <yujinsong.tech@gmail.com>
2024-07-31 20:55:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 184 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String word = sc.nextLine();
+        sc.close();
+        
+        System.out.println(word.length());
+    }
+}
