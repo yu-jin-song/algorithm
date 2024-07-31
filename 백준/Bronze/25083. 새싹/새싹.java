29ab68a235a54709521f086875465328d10925f5
Yujin Song <yujinsong.tech@gmail.com>
2024-07-31 21:16:09 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.lang.StringBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("         ,r\'\"7").append("\n")
+            .append("r`-_   ,'  ,/").append("\n")
+            .append(" \\. \". L_r\'").append("\n")
+            .append("   `~\\/").append("\n")
+            .append("      |").append("\n")
+            .append("      |");
+        
+        System.out.println(sb);
+    }
+}
