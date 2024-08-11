e58794440f42e645bc3de354cf0f0823ef83f0cd
Yujin Song <yujinsong.tech@gmail.com>
2024-08-11 21:53:58 +0900

    [Bronze III] Title: 약수 구하기, Time: 204 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        sc.close();
+        
+        int cnt = 0;
+        int result = 0;
+        for(int i = 1; i <= N; i++) {
+            if(N % i == 0 && ++cnt == K) {
+                result = i;
+                break;
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
