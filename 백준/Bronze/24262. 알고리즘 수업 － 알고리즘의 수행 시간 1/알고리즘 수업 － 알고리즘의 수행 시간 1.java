d58529b5a874f9bc05050c5f832c9759941e06b6
Yujin Song <yujinsong.tech@gmail.com>
2024-08-23 01:58:14 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        sc.close();
+        
+        System.out.println("1");
+        System.out.println("0");
+    }
+}
