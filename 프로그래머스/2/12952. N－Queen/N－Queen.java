7acfc489ef7d03cea44a4d98aa1684b8ee0726ba
joy <yujinsong.tech@gmail.com>
2025-03-03 23:27:34 +0900

    [level 2] Title: N-Queen, Time: 87.20 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,99 +1,41 @@
 class Solution {
-    private static class Block {
-        int y, x;
-        
-        public Block(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-    
     private static int N;
-    private static boolean[][] board;
+    private static boolean[] queen;
+    private static boolean[] diagonal1;
+    private static boolean[] diagonal2;
+    private static int maxCount;
     
     public int solution(int n) {
         N = n;
-        
-        int maxCount = 0;
-        for (int col = 0; col < n; col++) {
-            board = new boolean[n][n];
-            board[0][col] = true;   // 첫 행에 퀸 배치
-            if(dfs(1, col)) {
-                maxCount++;
-            }
-        }
+        queen = new boolean[N];
+        diagonal1 = new boolean[N*2];
+        diagonal2 = new boolean[N*2];
+        maxCount = 0;
+        dfs(0);
         return maxCount;
     }
     
-    // 2번째 행부터 마지막 행까지 dfs 진행
-    private static boolean dfs(int row, int col) {
-        // Block possiblePos = findEmptyPosition(row, col);
-        // if (possiblePos == null) {
-        //     return true; 
-        // }
-        
-        // int row = possiblePos.y;
-        // int col = possiblePos.x;
-        
-        // for (int y = row; y < N; y++) {
-        //     for (int x = col; x < N; x++) {
-                if (isValidRange(row, col) && isValid(row, col)) {
-                    // 현재 위치에 퀸 배치
-                    board[row][col] = true;
-                    
-                    if (dfs(row + 1, col)) {
-                        return true;
-                    }
-                }
-        //     }
-        // }
-        
-        return false;
-    }
-    
-    private static boolean isValid(int row, int col) {
-        if (isInRow(row) || isInColumn(col) || isInLeanCross(row, col)) {
-            return false;
+    private static void dfs(int row) {
+        if (row == N) {
+            maxCount++;
+            return;
         }
-        return true;
-    }
-    
-    private static boolean isInRow(int row) {
-        for (int x = 0; x < N; x++) {
-            if (board[row][x]) {
-                return true;
+        
+        // ↘
+        // 0 -> 2 -> 4 -> 6 {(0,0),(1,1),(2,2),(3,3)}
+        // 1 -> 3 -> 5  {(1,0),(2,1),(3,2)}
+        // (0,0) -> 2-1+4
+        for (int col = 0; col < N; col++) {
+            if (queen[col] || diagonal1[row+col] || diagonal2[col-row+N]) {
+                continue;
             }
-        }
-        return false;
-    }
-    
-    private static boolean isInColumn(int col) {
-        for (int y = 0; y < N; y++) {
-            if (board[y][col]) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    private static boolean isInLeanCross(int row, int col) {
-        if (isValidRange(row, col)) {
-            // 좌-상
-            boolean leftUp = isInLeanCross(row - 1, col - 1);
-            // 우-하
-            boolean rightDown = isInLeanCross(row + 1, col + 1);
             
-            if (leftUp && rightDown) {
-                return true;
-            }
+            // 퀸 배치, 우상-좌하↙ 대각선, 좌상-우하↘ 대각선 존재 표시
+            queen[col] = diagonal1[row+col] = diagonal2[col-row+N] = true;
+            // 다음행 탐색
+            dfs(row+1);
+            // 백트래킹
+            queen[col] = diagonal1[row+col] = diagonal2[col-row+N] = false;
         }
-        return false;
-    }
-    
-    private static boolean isValidRange(int row, int col) {
-        if (row >= 0 && row < N && col >= 0 && col < N) {
-            return true;
-        }
-        return false;
     }
 }
