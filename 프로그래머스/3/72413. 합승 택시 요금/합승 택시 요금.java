d1e1ccda2dcd957dbc3396ef67102e00c2033119
joy <yujinsong.tech@gmail.com>
2025-05-11 15:01:00 +0900

    [level 3] Title: 합승 택시 요금, Time: 54.53 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+
+class Solution {
+    private static class Node {
+        int dest, cost; // 도착노드, 비용
+        
+        public Node(int dest, int cost) {
+            this.dest = dest;
+            this.cost = cost;
+        }
+    }
+    
+    private static int N;
+    private static ArrayList<Node>[] nextNodes;
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        N = n;
+        
+        nextNodes = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            nextNodes[i] = new ArrayList<>();
+        }
+        
+        for (int[] fare : fares) {
+            nextNodes[fare[0]].add(new Node(fare[1], fare[2]));
+            nextNodes[fare[1]].add(new Node(fare[0], fare[2]));
+        }
+        
+        int[] sCost = search(s);    // 시작지점부터 각 노드까지의 비용
+        int[] aCost = search(a);    // a 지점부터 각 노드까지의 비용
+        int[] bCost = search(b);    // b 지점부터 각 노드까지의 비용
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            // 현재까지의 비용과 시작지점, a지점, b지점에서 현재 노드까지의 비용 중 적은 값으로 갱신
+            min = Math.min(min, sCost[i] + aCost[i] + bCost[i]);
+        }
+        
+        return min;
+    }
+    
+    private static int[] search(int start) {
+        int[] cost = new int[N + 1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        cost[start] = 0;    // 시작 지점의 비용은 0=
+        
+        ArrayDeque<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(start, 0));  // 시작 노드 설정
+        
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            
+            for (Node next : nextNodes[current.dest]) {
+                // if (!visited[next.dest]) {  // 방문하지 않은 노드인 경우만
+                    int newCost = cost[current.dest] + next.cost;
+                    
+                    // 새 비용이 현재 비용보다 저렴하다면
+                    if (newCost < cost[next.dest]) {
+                        cost[next.dest] = newCost;
+                        queue.add(new Node(next.dest, cost[next.dest]));
+                    }
+                // }
+            }
+        }
+        
+        return cost;
+    }
+}
